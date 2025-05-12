7a7ff421ae89fd8a286d77b1be6c5b7981312b44
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-05-12 14:29:06 +0300

    Authentication implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,22 @@
+import { InitialLayout } from '@/components/initialLayout'
 import { COLORS } from '@/constants/theme'
-import { Stack } from 'expo-router'
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+if (!publishableKey) {
+	throw new Error('Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in .env')
+}
 
 export default function RootLayout() {
 	return (
 		<SafeAreaProvider>
 			<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
-				<Stack screenOptions={{ headerShown: false }} />
+				<ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+					<InitialLayout />
+				</ClerkProvider>
 			</SafeAreaView>
 		</SafeAreaProvider>
 	)
--- constants/theme.ts
+++ constants/theme.ts
@@ -1,6 +1,6 @@
 export const COLORS = {
-	primary: '#4ca7af',
-	secondary: '#2db5d4',
+	primary: '#58a5c1',
+	secondary: '#26a1bc',
 	background: '#000000',
 	surface: '#1A1A1A',
 	surfaceLight: '#2A2A2A',
